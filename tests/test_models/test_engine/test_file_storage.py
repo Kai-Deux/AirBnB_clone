8c428b16f6b1d112f686a8099f567db912e0be57
yeshan111 <yeshan111@gmail.com>
2022-10-29 03:14:04 -0700

    pycodestyle update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -17,7 +17,7 @@ class TestFileStorageClassCreation(unittest.TestCase):
         self.file = 'file.json'
         try:
             os.remove(self.file)
-        except:
+        except Exception:
             pass
         self.x = BaseModel()
         self.fs = FileStorage()
@@ -26,7 +26,7 @@ class TestFileStorageClassCreation(unittest.TestCase):
     def tearDown(self):
         try:
             os.remove(self.file)
-        except:
+        except Exception:
             pass
 
     def test_inheritance(self):
